090881095dc13503b5a811d3b619bdb5287e2b77
n-o-tud <nikolas.ott@tu-dresden.de>
2024-04-12 11:21:42 +0200

    refactor: catch error if html element or data attribute not exist

--- src/store/mapAppConfig.js
+++ src/store/mapAppConfig.js
@@ -7,9 +7,17 @@ export const useMapAppConfig = defineStore("mapAppConfig", () => {
    * computed()s become getters
    * function()s become actions
    */
-  const el = document.querySelector("#whfd-mapping");
-  const dataUrl = ref(el.dataset.dataUrl);
-  const schemaUrl = ref(el.dataset.schemaUrl);
+  const el = ref(document.querySelector("#whfd-mapping"));
+  const dataUrl = ref(getDataAttributes("dataUrl"));
+  const schemaUrl = ref(getDataAttributes("schemaUrl"));
+
+  function getDataAttributes(attributeName) {
+    try {
+      return el.value.dataset[attributeName];
+    } catch (error) {
+      console.log("Attribute " + attributeName + " not found; " + error);
+    }
+  }
 
   function printOutMapAppConfig() {
     console.log("Map app config:");
