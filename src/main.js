788b5522efb914ee26e228b8dc4503f05e8dd132
n-o-tud <nikolas.ott@tu-dresden.de>
2024-07-10 14:41:45 +0200

    style: global style for maplibre and mapbox-draw

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-// import "@babel/polyfill";
-// import "mutationobserver-shim";
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// import global css styles
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.js'
+import 'maplibre-gl/dist/maplibre-gl.css'
+import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
 
 import { createPinia } from 'pinia'
 
