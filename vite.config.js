03af219ebce014460f26b13cda8fc85ba363a548
n-o-tud <nikolas.ott@tu-dresden.de>
2024-07-24 17:08:36 +0200

    test: install recommended lib for testing according to vuejs and adjust vitest config

--- vite.config.js
+++ vite.config.js
@@ -34,5 +34,12 @@ export default defineConfig(({ mode }) => {
         ]
       }
     }
+    // test: {
+    //   // enable jest-like global test APIs
+    //   globals: true,
+    //   // simulate DOM with happy-dom
+    //   // (requires installing happy-dom as a peer dependency)
+    //   environment: 'happy-dom'
+    // }
   }
 })
