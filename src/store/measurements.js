b21f26225a2b2931f88d15f05100be9190ced9a2
n-o-tud <nikolas.ott@tu-dresden.de>
2024-09-03 11:42:04 +0200

    refactor: adjust code according to noticeable problems figured out through testing

--- src/store/measurements.js
+++ src/store/measurements.js
@@ -14,7 +14,7 @@ export const useMeasurementStore = defineStore('measurements', () => {
   const geojson = ref(null)
   const dataSchema = ref(null)
   const dataVersion = ref(null)
-  const selectableProperties = ref(null)
+  const selectableProperties = ref([])
   const isDataLoading = ref(null)
   const isSchemaLoading = ref(null)
 
@@ -23,17 +23,15 @@ export const useMeasurementStore = defineStore('measurements', () => {
    * @param {String} url
    */
   async function fetchAPIDataSchema(url) {
-    //   /api/v1/schema/
     isSchemaLoading.value = true
     console.log('API Data Schema')
 
     try {
       await $RefParser.dereference(url).then((apiSchema) => {
         dataSchema.value = apiSchema.components.schemas.Measurement
-        console.log(dataSchema.value)
-        dataVersion.value = 'ghfdb' + apiSchema.info.version
+        dataVersion.value = 'ghfdb' + apiSchema.info.version //TODO: has to be adjusted with some real key describing the versioning
         console.log(dataVersion.value)
-        setSelectableProperties()
+        _setSelectableProperties(dataSchema.value)
         isSchemaLoading.value = false
       })
     } catch (error) {
@@ -42,12 +40,12 @@ export const useMeasurementStore = defineStore('measurements', () => {
   }
 
   /**
-   * @description
+   * @description recursiv function, navigation through paged api results. If no next page is linked, the collected results are retruned as array
    * @param {String} url
    * @param {Array} data
    * @returns {Array}
    */
-  async function fetchPagedAPIData(url, data) {
+  async function _fetchPagedAPIData(url, data) {
     data = data || []
 
     await axios
@@ -58,7 +56,7 @@ export const useMeasurementStore = defineStore('measurements', () => {
         })
 
         if (response.data.next != null) {
-          return fetchPagedAPIData(response.data.next, data)
+          return _fetchPagedAPIData(response.data.next, data)
         }
         return data
       })
@@ -73,77 +71,89 @@ export const useMeasurementStore = defineStore('measurements', () => {
    * @description Gather information of each site and write it to GeoJSON feature
    * @param {Object} siteObject
    */
-  function collectPntAttributes(siteObject) {
+  function _collectPntAttributes(siteObject) {
     const featKeys = Object.keys(siteObject)
 
-    let coord = null
-    let property = {}
+    let id = null
+    let geom = null
+    let prop = {}
 
     featKeys.forEach((key) => {
       if (key == 'sample') {
         if (siteObject[key].location != null) {
-          coord = siteObject[key].location.point
+          geom = siteObject[key].location.point
         }
+      } else if (key == 'id') {
+        id = siteObject[key]
       } else {
-        // console.log("hat keine location");
-        property[key] = siteObject[key]
+        prop[key] = siteObject[key]
       }
     })
 
-    return [coord, property]
+    return { id: id, geometry: geom, properties: prop }
   }
 
   /**
    * @description Write GeoJSON point feature and validate it
-   * @param {String} uuid
-   * @param {Array} coord
-   * @param {Object} property
+   * @param {Object} pntAttributes
    */
-  function writePntFeature(pntAttributes) {
+  function _writePntFeature(pntAttributes) {
+    // add geometry type of feature
+    pntAttributes.geometry['type'] = 'Point'
+
     const feature = {
-      id: pntAttributes[1].id,
+      id: pntAttributes.id,
       type: 'Feature',
-      geometry: pntAttributes[0],
-      properties: pntAttributes[1]
+      geometry: pntAttributes.geometry,
+      properties: pntAttributes.properties
     }
 
-    if (gjv.isFeature(feature, true)) {
-      return feature
-    } else {
-      console.log(gjv.isFeature(feature, true))
+    try {
+      if (gjv.isFeature(feature)) {
+        if (gjv.isPoint(feature.geometry)) {
+          return feature
+        }
+      }
+    } catch (error) {
+      console.log(error)
     }
   }
 
-  /**
+  /** HIER ÄNDERUNGEN GEMACHT VOR URLAUB
    * @description fetch JSON from heat flow API and convert it to geojson
    * @param {String} url
    */
-  function convertAPIData2GeoJSON(url) {
-    return fetchPagedAPIData(url).then((value) => {
-      console.log('measurments')
-      console.log(value)
-
-      let featuresArray = []
+  function _collectFeatures(url) {
+    return _fetchPagedAPIData(url).then((value) => {
+      let features = []
 
       value.forEach((siteObject) => {
-        // console.log(siteObject);
-        const pntAttributes = collectPntAttributes(siteObject)
-        featuresArray.push(writePntFeature(pntAttributes))
+        // transform into GeoJSON Point Feature
+        const pntAttributes = _collectPntAttributes(siteObject)
+        features.push(_writePntFeature(pntAttributes))
       })
-
-      const featureCollection = {
-        type: 'FeatureCollection',
-        features: featuresArray
-      }
-
-      if (gjv.isFeatureCollection(featureCollection)) {
-        console.log('inside async func')
-        console.log(featureCollection)
-        return featureCollection
-      } else {
-        console.log(gjv.isFeatureCollection(featureCollection, true))
-      }
+      return features
     })
+  }
+
+  /** HIER ÄNDERUNGEN GEMACHT VOR URLAUB
+   * @description
+   * @param {Array} features
+   * @returns
+   */
+  function _writeFeatureCollection(features) {
+    const featureCollection = {
+      type: 'FeatureCollection',
+      features: features
+    }
+
+    try {
+      if (gjv.isFeatureCollection(featureCollection)) {
+        return featureCollection
+      }
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   /**
@@ -154,7 +164,8 @@ export const useMeasurementStore = defineStore('measurements', () => {
     isDataLoading.value = true
     try {
       if (!localStorage.getItem(dataVersion)) {
-        geojson.value = await convertAPIData2GeoJSON(url)
+        const features = await _collectFeatures(url)
+        geojson.value = _writeFeatureCollection(features)
         localStorage.setItem(dataVersion, geojson)
       } else {
         geojson.value = localStorage.getItem(dataVersion)
@@ -171,23 +182,19 @@ export const useMeasurementStore = defineStore('measurements', () => {
    * @param {*} property
    * @returns {Boolean}
    */
-  function isPropertySelectable(property) {
+  function _isPropertySelectable(schema, property) {
     let isSelectable = null
 
-    if (
-      dataSchema.value.properties[property].type == 'string' &&
-      !dataSchema.value.properties[property].enum
-    ) {
+    if (schema.properties[property].type == 'string' && !schema.properties[property].enum) {
       console.log(property + ' is not suitable for data driven coloring')
       isSelectable = false
     } else if (
-      dataSchema.value.properties[property].type == 'integer' &&
-      (!dataSchema.value.properties[property].minimum ||
-        !dataSchema.value.properties[property].maximum)
+      schema.properties[property].type == 'integer' &&
+      (!schema.properties[property].minimum || !schema.properties[property].maximum)
     ) {
       console.log(property + ' is not suitable for data driven coloring')
       isSelectable = false
-    } else if (dataSchema.value.properties[property].type == 'object') {
+    } else if (schema.properties[property].type == 'object') {
       console.log(property + ' is not suitable for data driven coloring')
       isSelectable = false
     } else {
@@ -201,8 +208,8 @@ export const useMeasurementStore = defineStore('measurements', () => {
    * @param {String} propertyName
    * @returns {Object}
    */
-  function createVueMultiselectOption(propertyName) {
-    const propertyObj = dataSchema.value.properties[propertyName]
+  function _createVueMultiselectOption(schema, propertyName) {
+    const propertyObj = schema.properties[propertyName]
     let optionsObject = {}
     optionsObject['title'] = propertyObj.title
     optionsObject['key'] = propertyName
@@ -211,23 +218,18 @@ export const useMeasurementStore = defineStore('measurements', () => {
   }
 
   /**
-   * @description Throw out all properties options which are not suitable for the data driven coloring e.g. name, data points either be already classified (enum) or should be able to classify (continouse numerbs)
-   * @param {*} schema
-   * @returns {Array}
+   * @description Throw out all properties options which are not suitable for the data driven coloring e.g. name,
+   * data points either be already classified (enum) or should be able to classify (continouse numerbs). The attribute builds also the link between schema and a selection of users.
+   * They see the readable title and through the link of title corresponding key the selected attributes can easily be found in the schema like: dataSchema.properties[attributeKey]
    */
-  function setSelectableProperties() {
-    const propertiesKey = Object.keys(dataSchema.value.properties)
-    let selection = []
+  function _setSelectableProperties(schema) {
+    const propertyKeys = Object.keys(schema.properties)
 
-    propertiesKey.forEach((propertyName) => {
-      if (isPropertySelectable(propertyName)) {
-        selection.push(createVueMultiselectOption(propertyName))
+    propertyKeys.forEach((propertyName) => {
+      if (_isPropertySelectable(schema, propertyName)) {
+        selectableProperties.value.push(_createVueMultiselectOption(schema, propertyName))
       }
     })
-
-    selectableProperties.value = selection
-    console.log('check selectable properties')
-    console.log(selectableProperties.value)
   }
 
   return {
@@ -236,6 +238,12 @@ export const useMeasurementStore = defineStore('measurements', () => {
     selectableProperties,
     isDataLoading,
     isSchemaLoading,
+    _collectPntAttributes,
+    _writePntFeature,
+    _writeFeatureCollection,
+    _setSelectableProperties,
+    _isPropertySelectable,
+    _createVueMultiselectOption,
     fetchAPIData,
     fetchAPIDataSchema
   }
