3db1f58b0cb69ab5382f7a671c44e2165c1517dc
Sam <samuel.scott.jennings@gmail.com>
2024-10-24 16:47:21 +0200

    add VITE_APP_NAME and VITE_ROOT_API_DOMAIN env vars

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import VueDevTools from 'vite-plugin-vue-devtools'
 import { polyfillNode } from 'esbuild-plugin-polyfill-node'
+import { defineConfig, loadEnv } from 'vite'
+import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  // eslint-disable-next-line no-undef
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd())
   return {
     define: {
       'process.env': env
