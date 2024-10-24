0aa6b2d3c01c571043fdac5a958047d30b51b82c
n-o-tud <nikolas.ott@tu-dresden.de>
2024-10-24 15:44:56 +0200

    fix: typerror in third party lib with needed tot target the module

--- vite.config.js
+++ vite.config.js
@@ -3,26 +3,33 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import viteCompression from 'vite-plugin-compression'
 import { polyfillNode } from 'esbuild-plugin-polyfill-node'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // eslint-disable-next-line no-undef
-  const env = loadEnv(mode, process.cwd(), '')
+  // const env = loadEnv(mode, process.cwd(), '')
   return {
-    define: {
-      'process.env': env
-    },
+    // define: {
+    //   'process.env': env
+    // },
     assetsInclude: ['**/*.yaml', '**/*.geojson'],
-    plugins: [vue(), VueDevTools()],
+    plugins: [vue(), VueDevTools(), viteCompression()],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))
       }
     },
+    build: {
+      minify: false,
+      target: 'esnext'
+      // sourcemap: true
+    },
     optimizeDeps: {
       esbuildOptions: {
         // Node.js global to browser globalThis
+
         define: {
           global: 'globalThis'
         },
