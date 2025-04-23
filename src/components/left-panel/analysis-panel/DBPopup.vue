3489421222e060eee6f7dcc9552038de0caaca61
n-o-tud <nikolas.ott@tu-dresden.de>
2025-04-23 13:23:58 +0200

    feat: add layer handling buttons to graph (add and remove layer btn)

--- src/components/left-panel/analysis-panel/DBPopup.vue
+++ src/components/left-panel/analysis-panel/DBPopup.vue
@@ -1,7 +1,10 @@
 <!-- Show popup containing infos of point (on click) -->
 <script setup>
-import { defineProps, ref, watch } from 'vue'
+import { defineProps, h, onMounted, ref, render, watch } from 'vue'
 import { Map, Popup, Marker } from 'maplibre-gl'
+
+import DBAddLayerBtn from './DBAddLayerBtn.vue'
+import DBRemoveLastLayerBtn from './DBRemoveLastLayerBtn.vue'
 
 import { useDigitalBoreholeStore } from '@/store/digitalBorehole.js'
 
@@ -9,55 +12,66 @@ const dB = useDigitalBoreholeStore()
 
 const props = defineProps({ map: Map, hasPopup: Boolean })
 
-// TODO: when firt pnt is drawn popup does not toggle
-watch(
-  () => dB.pnt,
-  () => {
-    dB.bootstrapping(dB.layers, dB.t0, dB.closestPointfeatures.properties.q)
-    setUpPopup(dB.pnt, props.map)
-    dB.drawChart(dB.layers, dB.t0)
-  }
-)
-
-/**
- *
- */
-function setUpPopup() {
-  if (dB.popup && dB.marker) {
-    dB.popup.remove()
-    dB.popup = null
-    dB.marker.remove()
-    dB.marker = null
-  }
-
-  const elPopup = document.createElement('div')
-  elPopup.id = 'popupBoreholeChart'
-  elPopup.classList.add('mh-100')
-  dB.popup = ref(new Popup().setDOMContent(elPopup).setMaxWidth('100%'))
-
-  const elMarker = document.createElement('div')
-  elMarker.id = 'markerBoreholeChart'
-  elMarker.classList.add('fs-2')
-  elMarker.innerHTML =
-    '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrows-move" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708zM8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10M.146 8.354a.5.5 0 0 1 0-.708l2-2a.5.5 0 1 1 .708.708L1.707 7.5H5.5a.5.5 0 0 1 0 1H1.707l1.147 1.146a.5.5 0 0 1-.708.708zM10 8a.5.5 0 0 1 .5-.5h3.793l-1.147-1.146a.5.5 0 0 1 .708-.708l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L14.293 8.5H10.5A.5.5 0 0 1 10 8"></path></svg>'
-  dB.marker = ref(
-    new Marker({ draggable: true, element: elMarker, offset: [0, -10] }).setPopup(dB.popup)
+onMounted(() => {
+  // TODO: when firt pnt is drawn popup does not toggle
+  watch(
+    () => dB.pnt,
+    () => {
+      if (dB.pnt) {
+        setUpPopup(dB.pnt, props.map)
+        dB.marker.togglePopup()
+        dB.bootstrapping(dB.layers, dB.t0, dB.closestPointfeatures.properties.q)
+        dB.drawChart(dB.layers, dB.t0)
+        appendLayerHandlingBtnToPopup(document.getElementById('popupBoreholeChart'))
+      }
+    }
   )
-  // dB.marker.togglePopup()
 
-  dB.marker.setLngLat(dB.pnt.geometry.coordinates)
-  dB.marker.addTo(props.map)
-  dB.marker.togglePopup()
-}
+  /**
+   *
+   * @param parentElement
+   */
+  function appendLayerHandlingBtnToPopup(parentElement) {
+    if (parentElement) {
+      const col1 = h('div', { id: 'btn-col1', class: 'col-md-auto' }, [h(DBAddLayerBtn)])
+      const col2 = h('div', { id: 'btn-col2', class: 'col-md-auto' }, [h(DBRemoveLastLayerBtn)])
+      const row = h('div', { id: 'btn-row', class: 'row justify-content-md-center' }, [col1, col2])
+      const container = h('div', { id: 'db-layer-handling', class: 'container' }, [row])
+
+      render(container, parentElement)
+    }
+  }
+
+  /**
+   *
+   */
+  function setUpPopup() {
+    if (dB.popup && dB.marker) {
+      dB.popup.remove()
+      dB.popup = null
+      dB.marker.remove()
+      dB.marker = null
+    }
+
+    let elPopup = document.createElement('div')
+    elPopup.id = 'popupBoreholeChart'
+    elPopup.classList.add('mh-100')
+    dB.popup = ref(new Popup().setDOMContent(elPopup).setMaxWidth('100%'))
+    dB.popup.a
+
+    let elMarker = document.createElement('div')
+    elMarker.id = 'markerBoreholeChart'
+
+    dB.marker = ref(new Marker({ draggable: false, offset: [0, -10] }).setPopup(dB.popup))
+
+    dB.marker.setLngLat(dB.pnt.geometry.coordinates)
+    dB.marker.addTo(props.map)
+
+    // dB.marker.togglePopup()
+  }
+})
 </script>
 
 <template></template>
 
-<style>
-#markerBoreholeChart {
-  display: inline-block;
-  text-align: center;
-  line-height: 0; /* Remove any spacing around the SVG */
-  background: transparent;
-}
-</style>
+<style></style>
