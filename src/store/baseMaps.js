0272f7673bdc6906c7a75b04ca7dd6888939dde7
n-o-tud <nikolas.ott@tu-dresden.de>
2025-06-04 14:49:24 +0200

    feat: add new base maps

--- src/store/baseMaps.js
+++ src/store/baseMaps.js
@@ -32,6 +32,36 @@ export const useBaseMapsStore = defineStore('baseMaps', () => {
         'https://services.ga.gov.au/gis/rest/services/World_Bathymetry_Base_Map/MapServer/WMTS/tile/1.0.0/World_Bathymetry_Base_Map/default/default028mm/{z}/{y}/{x}.png',
       attribution:
         "Map tiles by <a href='https://ecat.ga.gov.au/geonetwork/srv/api/records/62030aca-329d-44b2-8b36-c74a5ac1f574'>World Bathymetry Base Map WMTS</a>, <a href='https://creativecommons.org/licenses/'>CC BY 4.0</a>"
+    },
+    {
+      id: 'bm_esri_world_physical',
+      title: 'ESRI World Physical',
+      cardImage:
+        'https://server.arcgisonline.com/ArcGIS/rest/services/World_Physical_Map/MapServer/tile/0/0/0.png',
+      tiles:
+        'https://server.arcgisonline.com/ArcGIS/rest/services/World_Physical_Map/MapServer/tile/{z}/{y}/{x}.png',
+      attribution: 'Tiles &copy; Esri &mdash; Source: US National Park Service'
+    },
+    {
+      id: 'bm_esri_world_imagery',
+      title: 'ESRI World Imagery',
+      cardImage:
+        'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/0/0/0.png',
+      tiles:
+        'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png',
+      attribution:
+        'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    },
+    {
+      id: 'bm_esri_ocean_basemap',
+      title: 'ESRI Ocean Basemap',
+
+      cardImage:
+        'https://server.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/0/0/0.png',
+      tiles:
+        'https://server.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}.png',
+      attribution:
+        'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri'
     }
   ])
 
