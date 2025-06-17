ba06abae15371727a1da36ba1871f680368220d3
n-o-tud <nikolas.ott@tu-dresden.de>
2025-06-17 16:21:06 +0200

    refactor: delete unused styles

--- src/components/map/MapCursorCoordinates.vue
+++ src/components/map/MapCursorCoordinates.vue
@@ -20,25 +20,10 @@ watch(isInstantiated, () => {
 
 <template>
   <div
-    class="badge rounded-pill text-bg-light position-absolute start-0 bottom-0 m-2 fs-6 z-2 bg-opacity-75 d-none d-md-block"
+    class="badge rounded-pill text-bg-light position-absolute start-0 bottom-0 m-2 fs-6 z-2 bg-opacity-50 d-none d-md-block mb-0 mb-md-3"
   >
     Lat: {{ lat }}, Lon: {{ lng }}
   </div>
 </template>
 
-<style scoped>
-.cursor-coordinates {
-  display: block;
-  position: relative;
-  margin: 0px auto;
-  width: fit-content;
-  padding: 10px;
-  border: none;
-  border-radius: 3px;
-  font-size: 10px;
-  text-align: left;
-  color: black;
-  background: rgba(255, 255, 255, 0.5);
-  z-index: 100;
-}
-</style>
+<style scoped></style>
