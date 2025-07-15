ee6518a883907d45e03e6517b1248f81bdcb49a1
n-o-tud <nikolas.ott@tu-dresden.de>
2025-07-15 10:56:43 +0200

    refactor: disable opacity when no modle is selected

--- src/components/left-panel/settings-panel/SettingsPanelDataModels.vue
+++ src/components/left-panel/settings-panel/SettingsPanelDataModels.vue
@@ -2,6 +2,7 @@
 import { ref, watch } from 'vue'
 
 import AccordionItem from '../AccordionItem.vue'
+import ModelAttrbution from './ModelAttrbution.vue'
 
 import { useSettingsStore } from '@/store/settings'
 import { useMapStore } from '@/store/map'
@@ -14,10 +15,19 @@ const hfModels = useHFModelsStore()
 watch(
   () => hfModels.selectedModel,
   () => {
-    if (hfModels.selectedModel) {
-      mapStore.map.setLayoutProperty('hf_r2024_grid', 'visibility', 'visible')
-    } else {
+    console.log(hfModels.selectedModel)
+    if (hfModels.selectedModel === 'none') {
+      Object.values(hfModels.models).forEach((model) => {
+        mapStore.map.setLayoutProperty(model.id, 'visibility', 'none')
+      })
       mapStore.map.setLayoutProperty('hf_r2024_grid', 'visibility', 'none')
+      return
+    }
+
+    if (hfModels.selectedModel) {
+      mapStore.map.setLayoutProperty(hfModels.selectedModel, 'visibility', 'visible')
+    } else {
+      mapStore.map.setLayoutProperty(hfModels.selectedModel, 'visibility', 'none')
     }
   }
 )
@@ -26,18 +36,37 @@ watch(
 <template>
   <!-- Circle Size  -->
   <AccordionItem title="Heat Flow Models" id="heaFlowModels">
-    <div v-for="model in hfModels.models" class="form-check">
+    <div class="form-check">
       <input
         class="form-check-input"
-        type="checkbox"
-        name="exampleRadios"
-        id="exampleRadios1"
-        value="option1"
+        type="radio"
+        name="modelRadio"
+        id="none"
+        value="none"
         v-model="hfModels.selectedModel"
       />
-      <label class="form-check-label" for="exampleRadios1"> {{ model.title }} </label>
+      <label class="form-check-label">none</label>
+      <div v-for="model in hfModels.models" :key="model.id">
+        <input
+          class="form-check-input"
+          type="radio"
+          name="modelRadio"
+          :id="model.id"
+          :value="model.id"
+          v-model="hfModels.selectedModel"
+        />
+        <label class="form-check-label" for="{{ model.id }}">
+          {{ model.title }}
+        </label>
+        <ModelAttrbution
+          :id="'model-attribution-' + model.title"
+          :attribution="model.attribution"
+          :doi="model.doi"
+        />
+      </div>
     </div>
-    <div v-if="hfModels.selectedModel">
+
+    <div v-if="hfModels.selectedModel && hfModels.selectedModel !== 'none'" class="form-check">
       <input
         type="range"
         class="form-range"
@@ -47,7 +76,11 @@ watch(
         max="100"
         v-model="hfModels.opacity"
         @input="
-          mapStore.map.setPaintProperty('hf_r2024_grid', 'raster-opacity', hfModels.opacity / 100)
+          mapStore.map.setPaintProperty(
+            hfModels.selectedModel,
+            'raster-opacity',
+            hfModels.opacity / 100
+          )
         "
       />
       <span class="text-muted text-center">{{ hfModels.opacity }} % Layer Opacity</span>
