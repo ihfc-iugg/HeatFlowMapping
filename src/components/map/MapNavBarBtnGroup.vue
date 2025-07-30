55eaed62b58da36647a1f873fe68ce7420c70284
n-o-tud <nikolas.ott@tu-dresden.de>
2025-07-30 11:14:10 +0200

    refactor: add data attributes to btn

--- src/components/map/MapNavBarBtnGroup.vue
+++ src/components/map/MapNavBarBtnGroup.vue
@@ -3,6 +3,11 @@ import { useNavigationBarStore } from '@/store/navigationBar'
 
 const navBar = useNavigationBarStore()
 
+/**
+ * @description Handles the interaction between the navigation bar and the offcanvas panel.
+ * @param {Object} clickedPanel
+ * @param navBar
+ */
 function controlNavbarOffcanvasInteraction(clickedPanel, navBar) {
   if (navBar.isCollapsed) {
     navBar.setPanelTitle(clickedPanel.title)
@@ -21,18 +26,19 @@ function controlNavbarOffcanvasInteraction(clickedPanel, navBar) {
 
 <template>
   <div id="map-navbar" class="btn-group bg-primary rounded-0 w-100 mb-0 mb-md-3" role="group">
-    <!-- <div v-for="item in navBar.navigationElements" :key="item.title"> -->
     <button
       v-for="item in navBar.navigationElements"
       :key="item.title"
       type="button"
       class="btn btn-primary flex-fill"
+      data-bs-toggle="offcanvas"
+      data-bs-target="#offcanvasPanel"
+      aria-controls="offcanvasPanel"
       @click="controlNavbarOffcanvasInteraction(item, navBar)"
     >
       <div v-html="item.svgElement"></div>
       <span class="">{{ item.title }}</span>
     </button>
-    <!-- </div> -->
   </div>
 </template>
 
@@ -44,7 +50,7 @@ function controlNavbarOffcanvasInteraction(clickedPanel, navBar) {
     left: 50%;
     transform: translateX(-50%);
     width: fit-content !important;
-    z-index: 1000; /* Ensure it is above other elements */
+    z-index: 1000;
     border-radius: var(--bs-border-radius) !important;
   }
 }
